6048342c8783d35fdb3138680528b223d45c381a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-03 15:42:08 +0200

    add test for desired behavior

--- tests/testthat/test-dots.R
+++ tests/testthat/test-dots.R
@@ -15,11 +15,14 @@ test_that("lazy_dots gobble empty last argument", {
 
   l1 <- lazy_dots(1,)
   l2 <- lazy_dots(1, 2, .follow_symbols = TRUE)
+  l3 <- lazy_dots(1,, .drop_last_if_empty = FALSE)
 
   expect_equal(l1[[1]]$expr, 1)
   expect_equal(length(l1), 1)
   expect_equal(l2[[1]]$expr, 1)
   expect_equal(l2[[2]]$expr, 2)
   expect_equal(length(l2), 2)
+  expect_equal(l3[[1]]$expr, 1)
+  expect_equal(length(l3), 2)
 
 })
