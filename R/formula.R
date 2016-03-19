78cb6a6612d591cbd7a5ed184793bff283d280da
Lionel Henry <lionel.hry@gmail.com>
2016-03-19 03:31:56 +0100

    Add extractors for two-sided formulas

--- R/formula.R
+++ R/formula.R
@@ -51,6 +51,35 @@ f_new <- function(expr, env = parent.frame()) {
   )
 }
 
+#' Extract parts of a two-sided formula
+#'
+#' These functions extract a one-sided formula from a two-sided one.
+#'
+#' @param f A two-sided formula
+#' @return A one-sided formula
+#' @export
+f_from_lhs <- function(f) {
+  check_two_sided(f)
+  f_new(f[[2]], env = environment(f))
+}
+
+#' @rdname f_from_lhs
+#' @export
+f_from_rhs <- function(f) {
+  check_two_sided(f)
+  f_new(f[[3]], env = environment(f))
+}
+
+check_two_sided <- function(f) {
+  if (!is_formula(f)) {
+    stop("`f` is a not a formula", call. = FALSE)
+  }
+
+  if (length(f) != 3) {
+    stop("`f` is not a two-sided formula", call. = FALSE)
+  }
+}
+
 #' Unwrap a formula
 #'
 #' This interpolates values in the formula that are defined in its environment,
